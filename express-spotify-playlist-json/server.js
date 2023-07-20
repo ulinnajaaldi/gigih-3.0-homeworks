15efac2d4adbd8ee70ca354575a2bc84af22759e
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2023-07-20 19:33:52 +0700

    refactor: add localhost:9000 when the server started

--- express-spotify-playlist-json/server.js
+++ express-spotify-playlist-json/server.js
@@ -7,5 +7,5 @@ app.use(playlistRoutes);
 
 const port = 9000;
 app.listen(port, () => {
-  console.log(`Server running on port http://${port}`);
+  console.log(`Server running on port http://localhost:${port}`);
 });
